6df1e5f68c48189196de98a303f089507fe5de5e
Alric42Mac <ale-cont@student.42lyon.fr>
2023-03-29 11:11:16 +0200

    up

--- srcs/philo_life.c
+++ srcs/philo_life.c
@@ -6,7 +6,7 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 13:34:52 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/03/27 22:16:21 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/03/29 10:08:11 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,12 +52,12 @@ void	life(t_philo *p)
 {
 	int	eat;
 
+	if (p->id % 2 == 0)
+		usleep(p->data->nb_philo * 100);
 	while ((p->meal_eaten != p->data->max_meal
 	|| p->data->max_meal == -1) && p->g_status)
 	{
 		eat = 1;
-		if (p->id % 2 == 0)
-			usleep(p->data->nb_philo * 100);
 		eat = p_eat(p);
 		if (eat > 1 || !eat)
 		{
